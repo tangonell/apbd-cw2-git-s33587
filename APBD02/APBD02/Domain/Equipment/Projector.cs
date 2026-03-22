260b021904eb1aa6b991c77b07afd7e253d77bc5
Tangonell <59707404+tangonell@users.noreply.github.com>
2026-03-22 22:45:15 +0100

    Update `Projector` to change `Resolution` type from `int` to `string`

--- APBD02/APBD02/Domain/Equipment/Projector.cs
+++ APBD02/APBD02/Domain/Equipment/Projector.cs
@@ -3,9 +3,9 @@ namespace APBD02.Domain.Equipment;
 public class Projector : Equipment
 {
     public int Brightness { get; }
-    public int Resolution { get; }
+    public string Resolution { get; }
     
-    public Projector(int id, string name, int brightness, int resolution) : base(id, name)
+    public Projector(int id, string name, int brightness, string resolution) : base(id, name)
     {
         Brightness = brightness;
         Resolution = resolution;
