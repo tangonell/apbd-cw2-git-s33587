c072ec2f5311f78057cf0f6680dd81b32982955a
Tangonell <59707404+tangonell@users.noreply.github.com>
2026-03-22 16:31:24 +0100

    Define a `Result` type

---
+++ APBD02/APBD02/Result/Result.cs
@@ -0,0 +1,37 @@
+namespace APBD02.Result;
+
+// Rust-like Result type
+
+public abstract record Result<T, E>
+{
+    public sealed record Ok(T Value) : Result<T, E>;
+    public sealed record Err(E Error) : Result<T, E>;
+
+
+    public T Expect(string msg) => this switch
+    {
+        Ok ok => ok.Value,
+        Err err => throw new InvalidOperationException($"{msg}: {err.Error}"),
+        _ => throw new InvalidOperationException("Unreachable")
+    };
+    
+    public T Unwrap() => this switch
+    {
+        Ok ok => ok.Value,
+        Err err => throw new InvalidOperationException($"called `Result::unwrap() on an `Err` value`: {err.Error}"),
+        _ => throw new InvalidOperationException("Unreachable")
+    };
+    
+    public T UnwrapOr(T defaultValue) => this switch
+    {
+        Ok ok => ok.Value,
+        Err _ => defaultValue,
+        _ => throw new InvalidOperationException("Unreachable")
+    };
+}
+
+public abstract record Result<E>
+{
+    public sealed record Ok : Result<E>;
+    public sealed record Err(E Error) : Result<E>;
+}
