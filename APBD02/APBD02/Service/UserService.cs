92749f53c52ad180c21911f2ef4d9bc52a79b74f
Tangonell <59707404+tangonell@users.noreply.github.com>
2026-03-22 19:41:02 +0100

    Create `UserService`

---
+++ APBD02/APBD02/Service/UserService.cs
@@ -0,0 +1,22 @@
+namespace APBD02.Service;
+
+using System.Collections.ObjectModel;
+using Domain.User;
+
+public class UserService
+{
+    private readonly List<User> _users = [];
+    private int _nextId = 1;
+
+    public T addUser<T>(Func<int, T> userFactory)
+        where T : User
+    {
+        var user = userFactory(_nextId);
+        _users.Add(user);
+        _nextId++;
+        
+        return user;
+    }
+    
+    public User? GetById(int id) => _users.FirstOrDefault(user => user.Id == id);
+}
