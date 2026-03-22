3c13550ce230f8e47d27b21e22f85008580f4672
Tangonell <59707404+tangonell@users.noreply.github.com>
2026-03-22 22:36:04 +0100

    Add return functionality and overdue calculation to `Rental`

--- APBD02/APBD02/Domain/Rental/Rental.cs
+++ APBD02/APBD02/Domain/Rental/Rental.cs
@@ -10,13 +10,21 @@ public class Rental
     public Equipment Equipment { get; }
     public DateTime RentalDate { get; }
     public DateTime DueDate { get; }
-    public DateTime? ReturnDate { get; }
-    public int Penalty { get; }
+    public DateTime? ReturnDate { get; private set; }
+    // Penalty is a fixed sum per overdue day
+    public decimal Penalty { get; }
+    
+    public bool IsActive => ReturnDate == null;
+    
+    // If not overdue, return 0
+    public int DaysOverdue => 
+        // ReturnDate cannot be null if the rental is not active; safe to suppress warning with '!'
+        Math.Max(0, ((IsActive ? DateTime.Now : ReturnDate!.Value) - DueDate).Days);
     
     public Rental(
         int id,
         User user, Equipment equipment,
-        DateTime rentalDate, DateTime dueDate, DateTime? returnDate,
+        DateTime rentalDate, DateTime dueDate,
         int penalty
     )
     {
@@ -25,6 +33,12 @@ public class Rental
         Equipment = equipment;
         RentalDate = rentalDate;
         DueDate = dueDate;
+        ReturnDate = null;
+        Penalty = penalty;
+    }
+
+    public void Return(DateTime returnDate)
+    {
         ReturnDate = returnDate;
     }
 }
